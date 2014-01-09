6c44f3fb8055825f997ea79d037c4b42899ad10e
Christian Tietze <christian.tietze@gmail.com>
2014-01-09 20:31:11 +0100

    Initial Commit

---
+++ AccessibilityObserver/CAXAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CAXAppDelegate.h
+//  AccessibilityObserver
+//
+//  Created by Christian Tietze on 09.01.14.
+//  Copyright (c) 2014 Christian Tietze. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface CAXAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ AccessibilityObserver/CAXAppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  CAXAppDelegate.m
+//  AccessibilityObserver
+//
+//  Created by Christian Tietze on 09.01.14.
+//  Copyright (c) 2014 Christian Tietze. All rights reserved.
+//
+
+#import "CAXAppDelegate.h"
+
+@implementation CAXAppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
