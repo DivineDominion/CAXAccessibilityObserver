af8e2a44541030d2d72c6af0df1800d365f9785d
Christian Tietze <christian.tietze@gmail.com>
2014-06-18 17:46:59 +0200

    somehow, adding this file slipped the last commits

---
+++ Source/CAXSystemPreferencesUtil.h
@@ -0,0 +1,15 @@
+//
+//  CAXSystemPreferencesUtil.h
+//  WordCounter
+//
+//  Created by Christian Tietze on 10.02.14.
+//  Copyright (c) 2014 Christian Tietze. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CAXSystemPreferencesUtil : NSObject
+
+- (void)openAccessibilitySystemPreferences;
+
+@end
